f39a6289bbdc0766d925ced59539a0e795ee3d1f
George_Maxwell <georgemaxwell.mail@gmail.com>
2022-07-30 04:31:40 +0500

    Clear PlayerController script and fix player "teleportation" on Cinemachine VCamera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D _playerRigitbody2D;
+    Rigidbody2D _playerRigidbody2D;
     Animator _playerAnimator;
 
     public bool playerCanMove = true;
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        _playerRigitbody2D = GetComponent<Rigidbody2D>();
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
         _playerAnimator = GetComponent<Animator>();
 
         instance = this;
@@ -29,41 +29,19 @@ public class PlayerController : MonoBehaviour
     {
         if (playerCanMove && !playerNeedsToStop)
         {
-            _playerRigitbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerMoveSpeed;
+            _playerRigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * playerMoveSpeed;
 
             GetComponent<SpriteRenderer>().flipX = Input.GetAxisRaw("Horizontal") < 0 ? true : false;
 
-            _playerAnimator.SetFloat("moveX", _playerRigitbody2D.velocity.x);
-            _playerAnimator.SetFloat("moveY", _playerRigitbody2D.velocity.y);
-
-            //if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
-            //{
-            //    _playerAnimator.SetBool("isMoving", false);
-            //}
-
+            _playerAnimator.SetFloat("moveX", _playerRigidbody2D.velocity.x);
+            _playerAnimator.SetFloat("moveY", _playerRigidbody2D.velocity.y);
         }
         else
         {
-            _playerRigitbody2D.velocity = Vector2.zero;
+            _playerRigidbody2D.velocity = Vector2.zero;
         }
 
-        if (Input.GetAxisRaw("Horizontal") == 1f ||
-            Input.GetAxisRaw("Horizontal") == -1f ||
-            Input.GetAxisRaw("Vertical") == 1f ||
-            Input.GetAxisRaw("Vertical") == -1f)
-        {
-            if (!playerNeedsToStop)
-            {
-                //_playerAnimator.SetBool("isMoving", true);
-                //if (playerCanMove)
-                //{
-                //    //определяем в какую сторону игрок будет смотреть когда остановится
-                //    _playerAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-                //    _playerAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
-                //}
-            }
-        }
-
+        //Clamp player in tilemap bounds. Bounds were set-up via CameraController.cs
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, _bottomLeftLevelLimit.x, _topRightLevelLimit.x),
                                          Mathf.Clamp(transform.position.y, _bottomLeftLevelLimit.y, _topRightLevelLimit.y),
                                          transform.position.z);
